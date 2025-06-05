d27aa7d3c4eef03a199cef09ffc46bf7d43caf2b
Krrish <146602915+Krrish-agrawal@users.noreply.github.com>
2025-06-05 09:50:08 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,13 @@ import { BrowserRouter,Switch, Route, Redirect} from "react-router-dom";
 import NavBar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+
 import PostDetails from "./components/PostDetails/PostDetails";
 const App=()=>{
     const user=JSON.parse(localStorage.getItem('profile'));
     return(
         <BrowserRouter>
-        <GoogleOAuthProvider
-        clientId=YOUR_GOOGLE_OAUTH_CLIENT_ID>
+       
         <Container maxWidth='lg'>
            <NavBar/>
             <Switch>
@@ -22,7 +21,7 @@ const App=()=>{
                 <Route path="/auth" exact component={()=>(!user?<Auth/>:<Redirect to="/posts"/>)}/>
             </Switch>
         </Container>
-        </GoogleOAuthProvider>
+       
         </BrowserRouter>
     )
 };
